0d3da9c57aa1960f1492fe1af57a06a2c56b9efb
akmaliftikar <35527514+akmaliftikar@users.noreply.github.com>
2020-05-02 21:06:21 +0700

    Create faq.php

---
+++ application/views/faq.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/bootstrap.css"/>
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/custom-navbar.css"/>
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/custom-landing-page.css"/>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/ico" href="<?php echo base_url(); ?>asset/image/icon-bar.png"/>
+
+  <title>Layanan | Eyecare</title>
+  <link rel="icon" type="image/ico" href="assets/eyecare-logo-blue.png"/>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg sticky-top">
+  <div class="container">
+    <a href="#"><img src="<?php echo base_url(); ?>asset/image/eyecare-logo-blue.png" alt="Logo" class="logo"></a>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link align-self-center" href="<?php echo base_url() ?>index.php/welcome">Home</a>
+        <a class="nav-item nav-link align-self-center" href="<?php echo base_url() ?>index.php/tentang">Tentang Kami</a>
+        <a class="nav-item nav-link align-self-center" href="<?php echo base_url() ?>index.php/layanan">Layanan</a>
+        <a class="nav-item nav-link align-self-center" href="<?php echo base_url() ?>index.php/cbook">Book Now</a>
+        <a class="nav-item nav-link active align-self-center" href="<?php echo base_url() ?>index.php/faq">FAQ</a>
+      </div>
+    </div>
+    <div class="d-flex justify-content-end">
+      <div class="align-self-center">
+        <!-- <a href="login.php" class="btn-masuk">Masuk</a> -->
+        <a href="<?php echo base_url() ?>index.php/clogin"><button class="btn btn-masuk" >Masuk</button></a>
+      </div>
+      <a href="<?php echo base_url() ?>index.php/cregist"><button class="btn btn-primary">Daftar</button></a>
+    </div>
+  </div>
+</nav>
+
+<section class="hero">
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col align-self-center">
+        <h2 style="text-align:center">FAQ</h2>
+        <br>
+        <h3>Cara mendaftar untuk berobat ke dokter spesialis</h3>
+        <p style = "text-align: justify" class="text"> 1. Pasien datang menuju ke counter spesialis</p>
+        <p style = "text-align: justify" class="text"> 2. Pasien Baru : Pasien / keluarga akan diminta untuk mengisi identitas pribadi sebagai data awal dalam pembuatan rekam medis pasien. Petugas akan menanyakan kepada pasien, apakah pasien memiliki fasilitas asuransi/jaminan perusahaan/pasien umum. Pasien akan dibuatkan kartu pasien, untuk dapat digunakan pada kunjungan selanjutnya. Jika pasien adalah pasien umum, maka sebelum pemeriksaan dilakukan, pasien membayar biaya pemeriksaan. Jika pasien adalah pasien asuransi/jaminan perusahaan maka pasien dapat memberikan kartu asuransi/jaminan perusahaan tersebut kepada petugas,petugas akan melakukan pengecekan biaya apa saja yang ditanggung oleh asuransi/perusahaan, termasuk melakukan konfirmasi terhadap asuransi/perusahaan tersebut, dan biaya pemeriksaan akan ditagihkan kepada asuransi/perusahaan tersebut. Pendaftaran poli spesialis buka sejak Pukul 07.00 – 21.00 WIB. </p>
+        <p style = "text-align: justify" class="text"> 3. Pasien Lama : Petugas akan menanyakan kepada pasien, apakah pasien sudah pernah berobat sebelumnya / sudah memiliki kartu pasien. Petugas akan menanyakan kepada pasien, apakah pasien memiliki fasilitas asuransi/jaminan perusahaan/pasien umum. Jika pasien sudah memiliki kartu pasien, maka petugas akan melakukan pendaftaran dengan memasukan NO. Rekam Medis pasien. Jika pasien adalah pasien umum, maka sebelum pemeriksaan dilakukan, pasien membayar biaya pemeriksaan. Jika pasien adalah pasien asuransi/jaminan perusahaan maka pasien dapat memberikan kartu asuransi/jaminan perusahaan tersebut kepada petugas, petugas akan melakukan pengecekan biaya apa saja yang ditanggung oleh asuransi/perusahaan, termasuk melakukan konfirmasi terhadap asuransi/perusahaan tersebut, dan biaya pemeriksaan akan ditagihkan kepada asuransi/perusahaan tersebut. Pendaftaran poli spesialis buka sejak Pukul 07.00 – 21.00 WIB</p>
+        
+        <h3>Pendaftaran Via Telepon</h3>
+        <p style = "text-align: justify" class="text"> Proses pendaftaran dapat dilakukan secara langsung (dengan cara pasien datang langsung ke bagian pendaftaran EyeCare) maupun secara tidak langsung (via Telepon). Pendaftaran via telefon dapat dilakukan dengan cara menghubungi No. Telp klinik EyeCare terdekat, kemudian menyebutkan hendak melakukan pendaftaran treatment yang mana, lalu petugas akan mendaftarkan pasien berdasarkan identitas pasien, sesuai dengan pelayanan yang dikendaki oleh pasien.</p>
+
+        <h3>Melakukan Perjanjian dengan dokter spesialis</h3>
+        <p style = "text-align: justify" class="text"> Perjanjian dengan dokter spesialis dapat dilakukan, baik secara langsung (pasien datang langsung ke klinik EyeCare untuk melakukan perjanjian pertemuan dengan dokter spesialis) maupun melakukan perjanjian secara tidak langsung dengan melalui telepon. Petugas akan membuatkan janji dengan dokter spesialis yang dipilih oleh pasien, sesuai dengan jadwal praktek dokter spesialis tersebut. Pasien bisa datang pada waktu yang telah disepakati dalam pejanjian tersebut untuk bertemu dengan dokter spesialis yang dimaksud. Perjanjian dapat dilakukan via telephone dengan menghubungi klinik EyeCare terdekat</p>
+        
+
+
+
+
+
+
+
+        
+        
+        <a href="<?php echo base_url() ?>index.php/cbook"><button class="btn btn-primary mt-4">Book Now <img class="panah" src="<?php echo base_url(); ?>asset/image/arrow-right.png"  alt="Arrow"></button></a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<section class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-3"><img src="<?php echo base_url(); ?>asset/image/eyecare-logo-white.png" alt="Logo Eyecare White"></div>
+      <div class="col-2">
+        <ul class="footer-menu">
+          <a href="<?php echo base_url() ?>index.php/tentang" style="color:white">Tentang Kami</f> <br> <br>
+          <a href="<?php echo base_url() ?>index.php/layanan" style="color:white">Layanan</a> <br> <br>
+          <a href="<?php echo base_url() ?>index.php/cbook" style="color:white">Book Now</a> <br> <br>
+          <a href="<?php echo base_url() ?>index.php/faq" style="color:white">FAQ</a>
+        </ul>
+      </div>
+      <div class="col-3">
+        <ul class="footer-menu">
+          <li>Kontak Kami</li>
+          <li>Syarat & Ketentuan</li>
+          <li>Kebijakan Privasi</li>
+        </ul>
+      </div>
+      <div class="col-4">
+        <ul class="footer-sosmed">
+          <li>Ikuti Kami</li>
+          <li>
+            <img src="<?php echo base_url(); ?>asset/image/instagram.png" alt="Instagram icon">
+            <img src="<?php echo base_url(); ?>asset/image/facebook.png" alt="Facebook icon">
+            <img src="<?php echo base_url(); ?>asset/image/twitter.png" alt="Twitter icon">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col text-center">
+        <hr class="hr-line">
+        <span >&copy; Eyecare 2020</span>
+      </div>
+    </div>
+  </div>
+</section>
+
+<script src="<?php echo base_url() ?>asset/js/bootstrap.js"></script>
+</body>
+</html>
