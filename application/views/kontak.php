c3747d598a16de8bf693f2c516d35fdd683f79c0
akmaliftikar <35527514+akmaliftikar@users.noreply.github.com>
2020-05-03 14:43:23 +0700

    Create kontak.php

---
+++ application/views/kontak.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/bootstrap.css"/>
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/custom-navbar.css"/>
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/custom-landing-page.css"/>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/ico" href="<?php echo base_url(); ?>asset/image/icon-bar.png"/>
+  <title>Kontak Kami | Klinik Mata Kepercayaan Anda</title>
+  
+</head>
+<body>
+<nav class="navbar navbar-expand-lg sticky-top">
+  <div class="container">
+    <a href="#"><img src="<?php echo base_url(); ?>asset/image/eyecare-logo-blue.png" alt="Logo" class="logo"></a>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link align-self-center" href="<?php echo base_url() ?>index.php/welcome">Home</a>
+        <a class="nav-item nav-link align-self-center" href="<?php echo base_url() ?>index.php/tentang">Tentang Kami</a>
+        <a class="nav-item nav-link align-self-center" href="<?php echo base_url() ?>index.php/layanan">Layanan</a>
+        <a class="nav-item nav-link align-self-center" href="<?php echo base_url() ?>index.php/cbook">Book Now</a>
+        <a class="nav-item nav-link align-self-center" href="<?php echo base_url() ?>index.php/faq">FAQ</a>
+      </div>
+    </div>
+    <div class="d-flex justify-content-end">
+      <div class="align-self-center">
+        <!-- <a href="login.php" class="btn-masuk">Masuk</a> -->
+        <a href="<?php echo base_url('index.php/clogin') ?>"><button class="btn btn-masuk">Masuk</button></a>
+      </div>
+      <a href="<?php echo base_url() ?>index.php/cregist"><button class="btn btn-primary">Daftar</button></a>
+    </div>
+  </div>
+</nav>
+
+<section class="hero">
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col align-self-center">
+        <h2>Kontak Kami</h2>
+        <br>
+        <h3> <img src = "<?php echo base_url(); ?>asset/image/phone.png" width="30" height="30" >  Call Center </h3>
+        <p class="text"> (021) 783 523 xxx </p>
+
+        <h3> <img src = "<?php echo base_url(); ?>asset/image/mobile.png" width="35" height="35" > SMS Center </h3>
+        <p class="text"> 0897 555 xxx </p>
+ 
+    
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-3"><img src="<?php echo base_url(); ?>asset/image/eyecare-logo-white.png" alt="Logo Eyecare White"></div>
+      <div class="col-2">
+        <ul class="footer-menu">
+          <a href="<?php echo base_url() ?>index.php/tentang" style="color:white">Tentang Kami</a> <br> <br>
+          <a href="<?php echo base_url() ?>index.php/layanan" style="color:white">Layanan</a> <br> <br>
+          <a href="<?php echo base_url() ?>index.php/cbook" style="color:white">Book Now</a> <br> <br>
+          <a href="<?php echo base_url() ?>index.php/faq" style="color:white">FAQ</a>
+        </ul>
+      </div>
+      <div class="col-3">
+        <ul class="footer-menu">
+          <a href="<?php echo base_url() ?>index.php/kontak" style="color:white">Kontak Kami</a> <br> <br>
+          <a href="<?php echo base_url() ?>index.php/disclaimer" style="color:white">Disclaimer</a> <br> <br>
+          <a href="<?php echo base_url() ?>index.php/kebijakanprivasi" style="color:white">Kebijakan Privasi</a>
+        </ul>
+      </div>
+      <div class="col-4">
+        <ul class="footer-sosmed">
+          <li>Ikuti Kami</li>
+          <li>
+            <img src="<?php echo base_url(); ?>asset/image/instagram.png" alt="Instagram icon">
+            <img src="<?php echo base_url(); ?>asset/image/facebook.png" alt="Facebook icon">
+            <img src="<?php echo base_url(); ?>asset/image/twitter.png" alt="Twitter icon">
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col text-center">
+        <hr class="hr-line">
+        <span >&copy; Eyecare 2020</span>
+      </div>
+    </div>
+  </div>
+</section>
+
+<script src="<?php echo base_url() ?>asset/js/bootstrap.js"></script>
+</body>
+</html>
